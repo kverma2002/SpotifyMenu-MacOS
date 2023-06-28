369c7b612cb79533c623bd009f129ef30788507f
Krit Verma <kverma@usc.edu>
2023-06-27 21:45:44 -0400

    Initial Commit

---
+++ APITest/APITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITestApp.swift
+//  APITest
+//
+//  Created by Krit Verma on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct APITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
